b4578001378606ba7d89bb2f9b82159a9454836a
icaro <insanaty.mightymare@gmail.com>
2013-03-08 02:01:44 -0300

    Alpha version esta por vir XD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G2f::Application.config.secret_token = 'd1baaf32c0082b3269e0e99e30dcbb76b123e3a241bd7d778948022f283cba11a8dd745404e514bcdd630fd0dc7bcbcc9cea49f8f8f1b38a1f9efa7d8dded8df'
